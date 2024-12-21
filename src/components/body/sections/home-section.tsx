eed64992d521e01633376d0fb7892da37779d455
Rakotomalala01 <johary.rakotmoalala2001@gmail.com>
2024-12-21 05:01:41 -0500

    JR: robot basic form

--- src/components/body/sections/home-section.tsx
+++ src/components/body/sections/home-section.tsx
@@ -1,6 +1,7 @@
 import ContactList from "@/components/ui/contact-list";
 import MyCVDownloader from "@/components/ui/myCVDownloader";
 import ProfileDescription from "@/components/ui/profil-description";
+import RobotHead from "@/components/ui/robot";
 
 const Home = () => {
 
@@ -10,24 +11,11 @@ const Home = () => {
       id="home"
       className="h-screen flex flex-col items-center justify-center text-center pb-10 bg-background text-white overflow-hidden"
     >
-      {/* <h1
-        className="text-4xl md:text-6xl font-blackops tracking-wider"
-        aria-live="polite"
-      >
-        <span className="">{text}</span>
-        <span
-          aria-hidden="true"
-          className={`text-keyword font-bold ${cursorVisible ? "opacity-100" : "opacity-0"}`}
-        >
-          |
-        </span>
-      </h1>
-      <p className="text-lg md:text-xl font-light text-muted-foreground mb-6 max-w-2xl">
-        {homeDescription}
-      </p> */}
+ 
       <ProfileDescription/>
       <MyCVDownloader/>
       <ContactList/>
+      <RobotHead/>
     </section>
   );
 };
---
+++ src/components/ui/robot.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const robotForm = "rounded-full mt-10 bg-gradient-to-br items-center justify-center from-gray-300 via-gray-400 to-blue-500 shadow-lg border-4 border-gray-500 w-12 h-12 md:w-16 md:h-16";
+
+const RobotHead = () => {
+    return (
+        <div className={robotForm}>
+                            
+        </div>
+    );
+  };
+
+export default RobotHead;
