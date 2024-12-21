b8c16373d8eef20623a8aa5f3f7974b34b75b06e
Rakotomalala01 <johary.rakotmoalala2001@gmail.com>
2024-12-21 04:05:11 -0500

    JR: add profil description component

--- src/components/body/sections/home-section.tsx
+++ src/components/body/sections/home-section.tsx
@@ -1,73 +1,16 @@
 import ContactList from "@/components/ui/contact-list";
 import MyCVDownloader from "@/components/ui/myCVDownloader";
-import { useState, useEffect, useMemo } from "react";
+import ProfileDescription from "@/components/ui/profil-description";
 
 const Home = () => {
 
-  // Typing configurations
-  const typingSpeed = 160; 
-  const deletingSpeed = 40; 
-  const delayBetweenTitles = 8000;
-  const homeDescription = "Passionate Software Engineer with expertise in AI and full-stack development. Skilled in building scalable systems and innovative solutions."
-
-  const titles = useMemo(
-    () => [
-      "Johary Rakotomalala",
-      "Software Engineer",
-      "Full-Stack Developer",
-      "Data Scientist",
-      "AI Enthusiast",
-      "Tech Innovator",
-    ],
-    []
-  );
-
-  const [currentTitleIndex, setCurrentTitleIndex] = useState(0);
-  const [text, setText] = useState("");
-  const [isDeleting, setIsDeleting] = useState(false);
-  const [cursorVisible, setCursorVisible] = useState(true);
-
-  useEffect(() => {
-    const currentTitle = titles[currentTitleIndex];
-
-    let timeout: NodeJS.Timeout;
-
-    if (isDeleting) {
-      if (text === "") {
-        setIsDeleting(false);
-        setCurrentTitleIndex((prevIndex) => (prevIndex + 1) % titles.length);
-      } else {
-        timeout = setTimeout(() => {
-          setText((prev) => prev.slice(0, -1));
-        }, deletingSpeed);
-      }
-    } else {
-      if (text === currentTitle) {
-        timeout = setTimeout(() => setIsDeleting(true), delayBetweenTitles);
-      } else {
-        timeout = setTimeout(() => {
-          setText((prev) => currentTitle.slice(0, prev.length + 1));
-        }, typingSpeed);
-      }
-    }
-
-    return () => clearTimeout(timeout);
-  }, [text, isDeleting, currentTitleIndex, titles]);
-
-  useEffect(() => {
-    const cursorInterval = setInterval(() => {
-      setCursorVisible((prev) => !prev);
-    }, 500);
-
-    return () => clearInterval(cursorInterval);
-  }, []);
 
   return (
     <section
       id="home"
       className="h-screen flex flex-col items-center justify-center text-center pb-10 bg-background text-white overflow-hidden"
     >
-      <h1
+      {/* <h1
         className="text-4xl md:text-6xl font-blackops tracking-wider"
         aria-live="polite"
       >
@@ -81,10 +24,10 @@ const Home = () => {
       </h1>
       <p className="text-lg md:text-xl font-light text-muted-foreground mb-6 max-w-2xl">
         {homeDescription}
-      </p>
+      </p> */}
+      <ProfileDescription/>
       <MyCVDownloader/>
       <ContactList/>
-      
     </section>
   );
 };
---
+++ src/components/ui/profil-description.tsx
@@ -0,0 +1,78 @@
+import  { useEffect, useMemo, useState } from 'react';
+
+const ProfileDescription = () => {
+    const typingSpeed = 160; 
+    const deletingSpeed = 40; 
+    const delayBetweenTitles = 8000;
+    const homeDescription = "Passionate Software Engineer with expertise in AI and full-stack development. Skilled in building scalable systems and innovative solutions."
+
+    const titles = useMemo(
+        () => [
+        "Johary Rakotomalala",
+        "Software Engineer",
+        "Full-Stack Developer",
+        "Data Scientist",
+        "AI Enthusiast",
+        "Tech Innovator",
+        ],
+        []
+    );
+
+    const [currentTitleIndex, setCurrentTitleIndex] = useState(0);
+    const [text, setText] = useState("");
+    const [isDeleting, setIsDeleting] = useState(false);
+    const [cursorVisible, setCursorVisible] = useState(true);
+
+    useEffect(() => {
+        const currentTitle = titles[currentTitleIndex];
+
+        let timeout: NodeJS.Timeout;
+
+        if (isDeleting) {
+        if (text === "") {
+            setIsDeleting(false);
+            setCurrentTitleIndex((prevIndex) => (prevIndex + 1) % titles.length);
+        } else {
+            timeout = setTimeout(() => {
+            setText((prev) => prev.slice(0, -1));
+            }, deletingSpeed);
+        }
+        } else {
+        if (text === currentTitle) {
+            timeout = setTimeout(() => setIsDeleting(true), delayBetweenTitles);
+        } else {
+            timeout = setTimeout(() => {
+            setText((prev) => currentTitle.slice(0, prev.length + 1));
+            }, typingSpeed);
+        }
+        }
+
+        return () => clearTimeout(timeout);
+    }, [text, isDeleting, currentTitleIndex, titles]);
+
+    useEffect(() => {
+        const cursorInterval = setInterval(() => {
+        setCursorVisible((prev) => !prev);
+        }, 500);
+
+        return () => clearInterval(cursorInterval);
+    }, []);
+  return (
+    <><h1
+          className="text-4xl md:text-6xl font-blackops tracking-wider"
+          aria-live="polite"
+      >
+          <span className="">{text}</span>
+          <span
+              aria-hidden="true"
+              className={`text-keyword font-bold ${cursorVisible ? "opacity-100" : "opacity-0"}`}
+          >
+              |
+          </span>
+      </h1><p className="text-lg md:text-xl font-light text-muted-foreground mb-6 max-w-2xl">
+              {homeDescription}
+          </p></>
+  );
+};
+
+export default ProfileDescription;
